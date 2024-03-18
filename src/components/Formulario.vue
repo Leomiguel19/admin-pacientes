f0100c135dc1aeb07db93897ef8a9f561b678449
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-03-17 21:25:06 -0400

    Finalizando Formulario

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -28,6 +28,70 @@
                     class="border-2 border-stone-300	w-full p-2 mt-2 placeholder-gray-400 rounded-md"
                 >
             </div>
+
+            <div class="mb-5">
+                <label 
+                    for="propietario" 
+                    class="block text-gray-700 uppercase font-bold"
+                >
+                    Nombre Propietario
+                </label>
+                <input 
+                    id="propietario"                    
+                    type="text"
+                    placeholder="Nombre de la propietario" 
+                    class="border-2 border-stone-300 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                >
+            </div>
+
+            <div class="mb-5">
+                <label 
+                    for="email" 
+                    class="block text-gray-700 uppercase font-bold"
+                >
+                    Email
+                </label>
+                <input 
+                    id="email"                    
+                    type="email"
+                    placeholder="Ingrese el email"  
+                    class="border-2 border-stone-300 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                >
+            </div>
+
+            <div class="mb-5">
+                <label 
+                    for="Alta" 
+                    class="block text-gray-700 uppercase font-bold"
+                >
+                    Fecha de Alta
+                </label>
+                <input 
+                    id="Alta"                    
+                    type="date"
+                    class="border-2 border-stone-300 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                >
+            </div>
+
+            <div class="mb-5">
+                <label 
+                    for="sintomas" 
+                    class="block text-gray-700 uppercase font-bold"
+                >
+                    Sintomas
+                </label>
+                <textarea
+                    id="sintomas"                                        
+                    placeholder="Describe los sintomas" 
+                    class="border-2 border-stone-300 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-40"
+                />
+            </div>
+
+            <input 
+                type="submit"
+                class="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-color"
+                value="Registrar Paciente"
+            >
         </form>
     </div>
 </template>
