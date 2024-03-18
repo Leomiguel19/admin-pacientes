81bc2aff6c44890427664094133476d960f083c3
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-03-17 20:57:55 -0400

    Crear componente formulario

---
+++ src/components/Formulario.vue
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<template>
+    <div class="md:w-1/2">
+        <h2 class="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+
+        <p class="text-lg mt-5 text-center mb-10">
+            Agrega pacientes y
+            <span class="text-indigo-600 font-bold">Administralos</span>
+        </p>
+    </div>
+</template>
