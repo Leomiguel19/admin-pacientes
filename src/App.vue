1da77c7317c70165ebc0829caf38ecb5bcd945a6
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-03-18 21:57:52 -0400

    Reiniciar el Formulario una vez agregado el paciente

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,23 @@
   }); 
 
   const guardarPaciente = () => {
-    pacientes.value.push(paciente)
+    pacientes.value.push({...paciente})
+
+    // Reiniciar el objeto
+    // paciente.nombre = '';
+    // paciente.propietario = '';
+    // paciente.email = '';
+    // paciente.alta = '';
+    // paciente.sintomas = '';
+
+    // Otra forma
+    Object.assign(paciente, {
+      nombre: '',
+      propietario: '',
+      email: '',
+      alta: '',
+      sintomas: ''
+    });
   }
 </script>
 
